58ffa8adb6da70a7e0179a6a62d759a573effb17
IlinSergey <Ilin_sergey90@mail.ru>
2022-09-15 21:02:38 +0300

    Переписал через классы, не много расширив возможности в виде ввода произаольного id пользователя и количества скачиваемых фото

--- main.py
+++ main.py
@@ -2,31 +2,44 @@ import requests
 import config
 from pprint import pprint
 
+class VkAgent:
+    def __init__(self, token:str, id:str):
+        self.token = token
+        self.id = id
 
-vk_token = config.vk_token
-url = 'https://api.vk.com/method/photos.get'
-params = {
-    'owner_id': '11606581',
-    'album_id': 'profile',
-    'extended': '1',
-    'photo_sizes': '1',
-    'rev': '1',
-    'access_token': vk_token,
-    'v': '5.131'
-}
-response = requests.get(url, params=params).json()
-#pprint(response)
+    def get_response(self, url, params):
+        return requests.get(url, params=params).json()
 
-def get_link(response = response, i=int):
-    for j in response['response']['items'][i]['sizes']:
-        if j['type'] == 'z':
-            return j['url']
-def get_photo():
-    for i in range(5):
-        file_name = response['response']['items'][i]['likes']['count']
-        link = get_link(response, i)
-        f = open(f'{file_name}.jpg', 'wb')
-        ufr = requests.get(link)
-        f.write(ufr.content)
-        f.close()
-# get_photo()
+
+    def get_link(self,response, i=int):
+        for j in response['response']['items'][i]['sizes']:
+            if j['type'] == 'z':
+                return j['url']
+
+    def get_photo(self, count:int):
+        url = 'https://api.vk.com/method/photos.get'
+        params = {
+            'owner_id': self.id,
+            'album_id': 'profile',
+            'extended': '1',
+            'photo_sizes': '1',
+            'rev': '1',
+            'access_token': self.token,
+            'v': '5.131'
+        }
+        response = self.get_response(url, params)
+        for i in range(count):
+            file_name = response['response']['items'][i]['likes']['count']
+            link = self.get_link(response, i)
+            f = open(f'backup\{file_name}.jpg', 'wb')
+            ufr = requests.get(link)
+            f.write(ufr.content)
+            f.close()
+        print('Фото скачены')
+
+token = config.vk_token
+vk = VkAgent(token, 11606581)
+vk.get_photo(2)
+
+
+
